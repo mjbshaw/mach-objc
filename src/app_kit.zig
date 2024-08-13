c7d8ca95aa7d6312c70d4d99c74dc5487bf63293
Michael Bradshaw <github@mjb.io>
2024-08-13 09:39:16 -0600

    Window and View

--- src/app_kit.zig
+++ src/app_kit.zig
@@ -1,46 +1,47 @@
+const ca = @import("quartz_core.zig");
 const ns = @import("foundation.zig");
 const objc = @import("objc.zig");
 
 /// `NSBackingStoreType`
 pub const BackingStoreType = enum(usize) {
     /// `NSBackingStoreRetained`
-    nsbacking_store_retained = 0,
+    retained = 0,
     /// `NSBackingStoreNonretained`
-    nsbacking_store_nonretained = 1,
+    nonretained = 1,
     /// `NSBackingStoreBuffered`
-    nsbacking_store_buffered = 2,
+    buffered = 2,
     _,
 };
 
 /// `NSWindowStyleMask`
-pub const WindowStyleMask = enum(usize) {
-    /// `NSWindowStyleMaskBorderless`
-    borderless = 0,
+pub const WindowStyleMask = packed struct(usize) {
     /// `NSWindowStyleMaskTitled`
-    titled = 1,
+    titled: bool = false,
     /// `NSWindowStyleMaskClosable`
-    closable = 2,
+    closable: bool = false,
     /// `NSWindowStyleMaskMiniaturizable`
-    miniaturizable = 4,
+    miniaturizable: bool = false,
     /// `NSWindowStyleMaskResizable`
-    resizable = 8,
-    /// `NSWindowStyleMaskTexturedBackground`
-    textured_background = 256,
-    /// `NSWindowStyleMaskUnifiedTitleAndToolbar`
-    unified_title_and_toolbar = 4096,
-    /// `NSWindowStyleMaskFullScreen`
-    full_screen = 16384,
-    /// `NSWindowStyleMaskFullSizeContentView`
-    full_size_content_view = 32768,
+    resizable: bool = false,
     /// `NSWindowStyleMaskUtilityWindow`
-    utility_window = 16,
+    utility_window: bool = false,
+    _padding1: u1 = 0,
     /// `NSWindowStyleMaskDocModalWindow`
-    doc_modal_window = 64,
+    doc_modal_window: bool = false,
     /// `NSWindowStyleMaskNonactivatingPanel`
-    nonactivating_panel = 128,
+    nonactivating_panel: bool = false,
+    _padding2: u4 = 0,
+    /// `NSWindowStyleMaskUnifiedTitleAndToolbar`
+    unified_title_and_toolbar: bool = false,
     /// `NSWindowStyleMaskHUDWindow`
-    hudwindow = 8192,
-    _,
+    hud_window: bool = false,
+    /// `NSWindowStyleMaskFullScreen`
+    full_screen: bool = false,
+    /// `NSWindowStyleMaskFullSizeContentView`
+    full_size_content_view: bool = false,
+    _padding3: u48 = 0,
+
+    pub const borderless = .{};
 };
 
 /// `NSResponder`
@@ -96,6 +97,51 @@ pub const Window = opaque {
     pub fn initWithContentRect_styleMask_backing_defer_screen(self: *@This(), content_rect: ns.Rect, style: WindowStyleMask, backing_store_type: BackingStoreType, flag: bool, screen: ?*Screen) *@This() {
         return objc.msgSend(self, "initWithContentRect:styleMask:backing:defer:screen:", *@This(), .{ content_rect, style, backing_store_type, flag, screen });
     }
+    /// `-[NSWindow isReleasedWhenClosed]`
+    pub fn releasedWhenClosed(self: *Window) bool {
+        return objc.msgSend(self, "isReleasedWhenClosed", bool, .{});
+    }
+    /// `-[NSWindow setReleasedWhenClosed:]`
+    pub fn setReleasedWhenClosed(self: *Window, release_when_closed: bool) void {
+        return objc.msgSend(self, "setReleasedWhenClosed:", void, .{release_when_closed});
+    }
+    /// `-[NSWindow contentView]`
+    pub fn contentView(self: *Window) *View {
+        return objc.msgSend(self, "contentView", *View, .{});
+    }
+    /// `-[NSWindow setContentView:]`
+    pub fn setContentView(self: *Window, view: *View) void {
+        return objc.msgSend(self, "setContentView:", void, .{view});
+    }
+    /// `-[NSWindow setIsVisible:]`
+    pub fn setIsVisible(self: *Window, visible: bool) void {
+        return objc.msgSend(self, "setIsVisible:", void, .{visible});
+    }
+    /// `-[NSWindow makeKeyAndOrderFront:]`
+    pub fn makeKeyAndOrderFront(self: *Window, sender: ?*objc.Id) void {
+        return objc.msgSend(self, "makeKeyAndOrderFront:", void, .{sender});
+    }
+};
+
+/// `NSView`
+pub const View = opaque {
+    pub const InternalInfo = objc.ExternClass("NSView", @This(), Responder);
+    pub const retain = InternalInfo.retain;
+    pub const release = InternalInfo.release;
+    pub const autorelease = InternalInfo.autorelease;
+    pub const as = InternalInfo.as;
+    /// `+[NSView new]`
+    pub const new = InternalInfo.new;
+    /// `+[NSView alloc]`
+    pub const alloc = InternalInfo.alloc;
+    /// `[[NSView alloc] init]`
+    pub const allocInit = InternalInfo.allocInit;
+    pub fn layer(self: *View) *ca.Layer {
+        return objc.msgSend(self, "layer", *ca.Layer, .{});
+    }
+    pub fn setLayer(self: *View, value: *ca.Layer) void {
+        return objc.msgSend(self, "setLayer:", void, .{value});
+    }
 };
 
 /// `NSScreen`
@@ -114,6 +160,10 @@ pub const Screen = opaque {
     /// `+[NSScreen screens]
     pub fn screens() *ns.Array(Screen) {
         return objc.msgSend(InternalInfo.class(), "screens", *ns.Array(Screen), .{});
+    }
+    /// `+[NSScreen mainScreen]
+    pub fn mainScreen() ?*Screen {
+        return objc.msgSend(InternalInfo.class(), "mainScreen", ?*Screen, .{});
     }
 };
 
